7087d3baab21d002b466c5b9998cc749d79bf0a3
Geoffrey Ritchey <geoffvr@live.com>
2018-03-02 12:40:15 -0600

    Update dailyWeather.py

--- dailyWeather.py
+++ dailyWeather.py
@@ -1,14 +1,14 @@
 import requests
 import pyttsx3
 
-url = 'http://api.openweathermap.org/data/2.5/weather?q=Lubbock&units=imperial&appid=9563a9db0bb1d5112facd4fd04c0279e'
+url = 'http://api.openweathermap.org/data/2.5/weather?q=Lubbock&units=imperial&appid=[appid here]'
 
 #openweathermap.org api
 json_data = requests.get(url).json()
 description = json_data['weather'][0]['description']
 temp = json_data['main']['temp']
 temp_max = json_data['main']['temp_max']
-sentence = "Good morning Geoffrey! The forecast today will be {}. The temperature is currently {} degrees Fahrenheit, with a high of {} degrees today".format(description, int(temp), int(temp_max))
+sentence = "Good morning! The forecast today will be {}. The temperature is currently {} degrees Fahrenheit, with a high of {} degrees today".format(description, int(temp), int(temp_max))
 print(sentence)
 
 #sets voice
